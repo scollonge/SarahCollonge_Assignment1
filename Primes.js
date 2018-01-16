22fa87031f53036256e232c873af000d069fc951
scollonge <35315876+scollonge@users.noreply.github.com>
2018-01-15 23:02:53 -0600

    Prime Solution

--- Primes.js
+++ Primes.js
@@ -8,21 +8,21 @@ function IsPrime(value) {
         return false;
     if (value == 2)
         return true;
-    else for (var i = 2; i < value; i++) {
-        var remainder = value % i;
-        if (remainder === 0) {
-            return false;
+    else {
+        for (var i = 2; i < value; i++) {
+            if (value % i === 0) {
+                return false;
+            }
         }
         return true;
     }
 }
 
-
-for (var number = 2, primeCount = 1; ; number++) {
-    if (IsPrime(number) == true)
+for (var primeCount = 0, number = 2; ; IsPrime(number++)) {
+    if (IsPrime(number) == true) {
         primeCount++
-    if primeCount == 1000{
-        break
-        console.log(number)
+        console.log(number, primeCount)
     }
+    if (primeCount == 1000)
+        break
 }
