dfb38ad5bab9b19e673d6d69016a34263da7ed07
scollonge <35315876+scollonge@users.noreply.github.com>
2018-01-15 21:19:50 -0600

    Fizz Buzz Solution

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,7 +5,7 @@ print "Buzz".
 For numbers which are multiples of both three and five print "FizzBuzz"
  */
 
-for ( var i = 0; i < 100; i++){
+for ( var i = 1; i < 101; i++){
     if (i % 15 == 0)
         console.log('FizzBuzz')
     else if (i % 3 == 0)
